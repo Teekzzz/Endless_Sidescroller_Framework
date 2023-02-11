6afb4801dd91a06a3733c633344cf07bc2655d37
Teekzzz <124139969+Teekzzz@users.noreply.github.com>
2023-02-11 03:15:34 +0000

    Add files via upload

--- game.js
+++ game.js
@@ -1,152 +1,201 @@
-//Set the obstacle element to be generated when the game starts and when previous obstacles leave the screen.
-const obstacle = document.querySelector('.obstacle');
-let obstacleLeft = 100;
+//Import JSFiddle
 
+
+
+// Declaring the constants for the obstacle, player, and score.
+
+const obstacle = document.querySelector('.obstacle');
+const obstacleArray = [];
 
 const player = document.querySelector('.player');
 let playerLeft = 0;
 let playerBottom = 0;
 let jumping = false;
+let doubleJumping = false;
+
+// Animate the Player
+let currentFrame = 0;
+const frameWidth = 123;
+const frameHeight = 123;
+const numFrames = 8;
+
+function animatePlayer() {
+    currentFrame = (currentFrame + 1) % numFrames;
+    player.style.backgroundPosition = `-${currentFrame * frameWidth}px 0px`;
+}
+    
+    
+
+
 
 const scoreDisplay = document.querySelector('.score');
 let score = 0;
 let startTime = Date.now();
 
-const obstacleArray = [];
+
+// Creating the obstacle and randomizing the obstacle spawn.
 
 function createObstacle() {
-    //Code for creating a new obstacle element
-    const newObstacle = () => {
-        //Code for creating a new obstacle element
-        const newObstacle = document.createElement('div');
-        newObstacle.classList.add('obstacle');
-        newObstacle.style.top = `${Math.random() * window.innerHeight}px`;
-        newObstacle.style.left = `${window.innerWidth + score}px`;
-        newObstacle.style.width = 50 + 'px';
-        newObstacle.style.height = 50 + 'px';
-        document.body.appendChild(newObstacle);
+    const newObstacle = document.createElement('div');
+    gameContainer.appendChild(newObstacle);
+    newObstacle.classList.add('obstacle');
 
-        return newObstacle;
-      };    
-    
-    obstacleArray.push(newObstacle); 
+    // Generate a random number between 0 and 2
+
+    const randomNum = Math.floor(Math.random() * 3);
+
+    switch (randomNum) {
+      case 0:
+        newObstacle.style.bottom = "0px";
+        break;
+      case 1:
+        newObstacle.style.bottom = "50px";
+        break;
+      case 2:
+        newObstacle.style.bottom = "100px";
+        break;
+    }
+
+    newObstacle.style.left = window.innerWidth;
+    newObstacle.style.width = '50px';
+    newObstacle.style.height = '50px';
+  
+    let speed = 2 + score / 20;
+    newObstacle.speed = speed;
+
+    return newObstacle;
 };
 
-createObstacle();
-      
-setInterval(() => createObstacle(), 1000);
+function randomizeObstacleSpawn() {
+    const randomTimeout = Math.floor(Math.random() * 2000) + 500;
+    setTimeout(() => {
+      obstacleArray.push(createObstacle());
+      if (obstacle.offsetLeft + obstacle.offsetWidth < 0) {
+        removeObstacle(obstacleArray[index]);
+        index++;
+      }
+      randomizeObstacleSpawn();
+    }, randomTimeout);
+  }
+  
+  randomizeObstacleSpawn();
 
+// Creating the player movement and collision detection.
 
-// Set the game container to be the same height as the window
+const moveObstacle = (obstacle) => {
+    obstacle.style.left = `${obstacle.offsetLeft - obstacle.speed}px`;
+};
+
 const gameContainer = document.querySelector('#game-container');
-gameContainer.style.height = (window.innerHeight) + 'px';
-gameContainer.style.width = (window.innerWidth) + 'px';
-gameContainer.background = 'black';
-gameContainer.style.width = 100 + '%';
+gameContainer.style.height = `${window.innerHeight}px`;
+gameContainer.style.width = '100%';
+gameContainer.style.background = 'black';
 
-//Set the position of the player
 player.style.left = '0';
 player.style.bottom = '0';
 
-
-
-//Move the player using the A, D and Space Keys
-
 document.addEventListener('keydown', (e) => {
+    if (checkCollision() === true) return; // Exit the game early, and prevent post-death movement if the player has lost.
+
     if (e.code === 'KeyA') {
         playerLeft -= 2;
-        player.style.left = playerLeft + '%';
+        player.style.left = `${playerLeft}%`;
+        animatePlayer();
     } else if (e.code === 'KeyD') {
         playerLeft += 2;
-        player.style.left = playerLeft + '%';
+        player.style.left = `${playerLeft}%`;
+        animatePlayer();
     } else if (e.code === 'Space' && !jumping) {
         jumping = true;
         playerBottom += 100;
-        player.style.bottom = playerBottom + 'px';
+        player.style.bottom = `${playerBottom}px`;
         setTimeout(() => {
             playerBottom -= 100;
-            player.style.bottom = playerBottom + 'px';
+            player.style.bottom = `${playerBottom}px`;
             jumping = false;
-        }, 500);
+            doubleJumping = false;
+        }, 1000);
+    } else if (e.code === 'Space' && jumping && !doubleJumping) {
+        doubleJumping = true;
+        playerBottom += 100;
+        player.style.bottom = `${playerBottom}px`;
+        setTimeout(() => {
+            playerBottom -= 100;
+            player.style.bottom = `${playerBottom}px`;
+            jumping = false;
+            doubleJumping = false;
+        }, 1000);
+    }
+
+    //Prevent the player from being able to move off-screen (or outside of the viewport).
+
+    if (playerLeft < 0) {
+        playerLeft = 0;
+    }
+    if (playerLeft > 95) {
+        playerLeft = 95;
     }
 });
 
-// Move the obstacles towards the player at varying speeds, that get increasingly faster as the players score increases
-const moveObstacle = () => {
-    obstacleLeft -= 2 + score / 50;
-    obstacle.style.left = obstacleLeft + '%';
-};
 
-// Check for collision between the player and the obstacle
 const checkCollision = () => {
     const playerRect = player.getBoundingClientRect();
-    let collision = false;
-    const obstacleRect = obstacle.getBoundingClientRect();
 
-    if (
-        playerRect.right > obstacleRect.left &&
-        playerRect.left < obstacleRect.right &&
-        playerRect.bottom > obstacleRect.top &&
-        playerRect.top < obstacleRect.bottom
-    ) {   
-        scoreDisplay.innerHTML = `Game Over! : ${score.toFixed(1)}`;
-        return true;
+    for (const obstacle of obstacleArray) {
+        const obstacleRect = obstacle.getBoundingClientRect();
+
+        if (
+            playerRect.right > obstacleRect.left &&
+            playerRect.left < obstacleRect.right &&
+            playerRect.bottom > obstacleRect.top &&
+            playerRect.top < obstacleRect.bottom
+        ) {
+            scoreDisplay.innerHTML = `Game Over! : ${score.toFixed(1)}`;
+            return true;
+        }
     }
 
     return false;
 };
 
-// Stop the game when an obstacle is hit and display a reset button to restart the game
+
+// Declaring the stop game, update score and removeObstacle functions. (I know I should move this higher up).
+
 const stopGame = () => {
     clearInterval(gameLoop);
-    const resetButton = document.querySelector('.reset-button');
-    resetButton.innerHTML = 'Reset';
-    resetButton.addEventListener('click', () => {
-        window.location.reload();
-    });
-    document.body.appendChild(resetButton);
 };
 
-// When an obstacle goes off the screen, remove the obstacle and create a new one
-let currentObstacle = obstacle;
-
-const removeObstacle = () => {
-    currentObstacle.remove();
-    currentObstacle = createObstacle();
-};
-
-// Prevent the play from moving off the screen.
-const checkObjective = () => {
-    if (playerLeft < 0) {
-        playerLeft = 0;
-    } else if (playerLeft > 100) {
-        playerLeft = 0;
+const removeObstacle = (obstacle) => {
+    if (obstacle.offsetLeft + obstacle.offsetWidth < 0) {
+      obstacle.remove();
     }
+}; 
 
+const updateScore = () => {
+    score = (Date.now() - startTime) / 1000;
+    scoreDisplay.innerHTML = `Score: ${score.toFixed(1)}`;
 };
 
-//The main game loop where all the actions occur
-const gameLoop = setInterval(() => {
-    const timeElapsed = (Date.now() - startTime) / 1000;
-    score = timeElapsed;
-    scoreDisplay.innerHTML = score.toFixed(1);
 
-    if(checkObjective()) {
-        clearInterval(gameLoop);
-    } else {
-        moveObstacle();
-        checkCollision();
+// Declaring the start game function and calling it.
+
+let gameLoop;
+    
+const startGame = () => {
+    score = 0;
+    startTime = Date.now();
+    gameLoop = setInterval(() => {
+    for (const obstacle of obstacleArray) {
+        moveObstacle(obstacle);
     }
-
+    updateScore();
     if (checkCollision()) {
         stopGame();
-        clearinterval(gameLoop);
     }
+    }, 16);
 
-    if (currentObstacle.offsetLeft < -10) {
-        removeObstacle();
-    }
-}, 20);
+    animateScript();
 
-//Start the Game
-gameLoop;
+    };
+    
+startGame();
